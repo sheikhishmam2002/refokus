a2df24d0aeacf5df5556ff3217d47f76bef6e4ca
sheikhishmam2002 <latifurahman2@gmail.com>
2024-12-20 06:54:07 -0800

    Updation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
       <div className="max-w-screen-xl mx-auto py-10 flex gap-32">
         <div className="basis-1/2 ">
           <h1 className="text-[11.5rem] font-semibold leading-none tracking-tight">
-            refocus.
+            refokus.
           </h1>
         </div>
         <div className="basis-1/2 flex gap-4">
